90bf1c0a6050db6bf16c4960b71ffbf1a78d7448
EshaShetty <esha.dshetty@gmail.com>
2024-02-14 19:32:43 -0500

    setting up dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,18 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
     namespace = "com.eshashetty.baseproject"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.eshashetty.baseproject"
-        minSdk = 25
-        targetSdk = 33
+        minSdk = 30
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -36,11 +38,53 @@ android {
 }
 
 dependencies {
+    dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+        // AndroidX
+        implementation("androidx.core:core-ktx:1.12.0")
+        implementation("androidx.appcompat:appcompat:1.6.1")
+        implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+        implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+        implementation("androidx.legacy:legacy-support-v4:1.0.0")
+
+        // Material UI
+        implementation("com.google.android.material:material:1.11.0")
+
+        // Coroutines
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+        implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+
+        // Network
+        val okhttpVersion = "4.9.3"
+        val retrofitVersion = "2.9.0"
+        implementation("com.squareup.retrofit2:retrofit:${retrofitVersion}")
+        implementation("com.squareup.retrofit2:converter-gson:${retrofitVersion}")
+        implementation("com.squareup.retrofit2:adapter-rxjava2:${retrofitVersion}")
+        implementation("com.squareup.okhttp3:okhttp:${okhttpVersion}")
+        implementation("com.squareup.okhttp3:okhttp-urlconnection:${okhttpVersion}")
+        implementation("com.squareup.okhttp3:logging-interceptor:${okhttpVersion}")
+
+        //Hilt - DI
+        val hiltVersion = "2.44"
+        implementation("com.google.dagger:hilt-android:${hiltVersion}")
+        kapt("com.google.dagger:hilt-android-compiler:${hiltVersion}")
+
+        // Glide
+        implementation("com.github.bumptech.glide:glide:4.14.2")
+        kapt("com.github.bumptech.glide:compiler:4.13.0")
+
+        // Testing
+        testImplementation("junit:junit:4.13.2")
+        testImplementation("org.robolectric:robolectric:4.9.2")
+        testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0")
+        testImplementation("org.mockito:mockito-core:4.5.1")
+        testImplementation("org.mockito:mockito-inline:4.1.0")
+        testImplementation("io.mockk:mockk:1.12.0")
+        testImplementation("androidx.test:core:1.5.0")
+        testImplementation("androidx.test.ext:junit:1.1.5")
+        testImplementation("androidx.arch.core:core-testing:2.2.0")
+        testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.1")
+
+    }
 }
